7b18790fe2ab9aeada4d81fc00fd96731411d98f
moxiaoming <maiqiming487@qq.com>
2018-11-15 22:16:38 +0800

    改了些注释

--- MyLibrary/lib-aop/src/main/java/zs/xmx/aop/login/callback/ILoginCallback.java
+++ MyLibrary/lib-aop/src/main/java/zs/xmx/aop/login/callback/ILoginCallback.java
@@ -8,7 +8,9 @@ import android.content.Context;
  * @创建时间   2018/11/8 11:23
  * @本类描述	  登录状态回调接口
  * @内容说明
- *
+ * todo 参考下takephoto库,能不能也能类实现这个接口就能拿到数据
+ * todo 其实就是MVP,P层拿到V层接口而已,然后传递数据到接口方法而已
+ * todo 不过如果是组件化项目,BaseActivity处理完,不好路由跳转(因为路由常量路径,不在BaseLibrary里面)
  */
 public interface ILoginCallback {
 
